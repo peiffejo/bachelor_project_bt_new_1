d180384a66e7bea940a6714d69da76898c0b5ddc
peiffejo <johannes-hubert.peiffer@informatik.hs-fulda.de>
2023-03-01 13:38:58 +0100

    test

--- example/lib/ChatPage.dart
+++ example/lib/ChatPage.dart
@@ -1,9 +1,20 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'dart:math';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'package:location/location.dart';
+
+
+int waitIfBigger = 10;
+double checkDistancOne = 0.0;
+int positionBySteps =0;
+String origin = "";
+String destination= "";
+
 
 class ChatPage extends StatefulWidget {
   final BluetoothDevice server;
@@ -22,14 +33,17 @@ class _Message {
 }
 
 class _ChatPage extends State<ChatPage> {
-  static final clientID = 0;
+  final Location _location = Location();
+  LocationData? currentLocation;
+  List<dynamic> stepsCordi = [];
+  List<dynamic> maneuver = [];
+  List<dynamic> dis = [];
+  List<dynamic> steps = [];
   BluetoothConnection? connection;
 
   List<_Message> messages = List<_Message>.empty(growable: true);
   String _messageBuffer = '';
 
-  final TextEditingController textEditingController =
-      new TextEditingController();
   final ScrollController listScrollController = new ScrollController();
 
   bool isConnecting = true;
@@ -37,10 +51,186 @@ class _ChatPage extends State<ChatPage> {
 
   bool isDisconnecting = false;
 
+//getroute
+  Future<void> getRoute() async {
+    if(stepsCordi.isEmpty) {
+      String apiKey = "";
+      String url =
+          "https://maps.googleapis.com/maps/api/directions/json?origin=$origin&destination=$destination&mode=walking&key=$apiKey";
+      var response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        Map<String, dynamic> data = json.decode(response.body);
+        if (data.isEmpty) {
+          print("Somthing went wrong with URL request");
+        }
+        steps = await data['routes'][0]['legs'][0]['steps'];
+      }
+
+      print("Should be printed 1 time");
+      maneuver = [];
+      dis = [];
+
+    }
+
+    print(stepsCordi);
+
+    // all possible maneuver
+    //     turn-right: The driver should turn right at the next intersection.
+    //     turn-left: The driver should turn left at the next intersection.
+    //     slight-right: The driver should bear slightly to the right at the next intersection.
+    //     slight-left: The driver should bear slightly to the left at the next intersection.
+    //     ramp-right: The driver should turn or exit to the right onto a ramp or slip road.
+    //     ramp-left: The driver should turn or exit to the left onto a ramp or slip road.
+    //     fork-right: The driver should bear to the right at a fork in the road where two or more separate roads diverge from a common point.
+    //     fork-left: The driver should bear to the left at a fork in the road where two or more separate roads diverge from a common point.
+    //     merge: Two separate roadways become one, and the driver is instructed to safely blend their vehicle into the flow of traffic on the combined roadway.
+    //     roundabout-left: The driver should navigate the roundabout in a counterclockwise direction until they reach their desired exit on the left.
+    //     roundabout-right: The driver should navigate the roundabout in a clockwise direction until they reach their desired exit on the right.
+    //     roundabout-alternative: The driver should take an alternative route that bypasses the roundabout.
+
+    if(maneuver.isEmpty) {
+      for (int i = 0; i < steps.length; i++) {
+        stepsCordi.add(LatLng(steps[i]['start_location']['lat'],
+            steps[i]['start_location']['lng']));
+        print(steps[i]['maneuver']);
+        if (steps[i]['maneuver'] == "straight") {
+          maneuver.add('W');
+        }
+        if (steps[i]['maneuver'] == "turn-left") {
+          maneuver.add('S');
+        }
+        if (steps[i]['maneuver'] == "turn-slight-right") {
+          maneuver.add('E');
+        }
+        if (steps[i]['maneuver'] == "turn-right") {
+          maneuver.add('D');
+        }
+        if (steps[i]['maneuver'] == "sharp-right") {
+          maneuver.add('D');
+        }
+        if (steps[i]['maneuver'] == "U-turn") {
+          maneuver.add('S');
+        }
+        if (steps[i]['maneuver'] == "turn-sharp-left") {
+          maneuver.add('A');
+        }
+        if (steps[i]['maneuver'] == "turn-left") {
+          maneuver.add('A');
+        }
+        if (steps[i]['maneuver'] == "turn-slight-left") {
+          maneuver.add('Q');
+        }
+        if (steps[i]['maneuver'] == "arrive-at-destination") {
+          maneuver.add('2');
+        }
+        if (steps[i]['maneuver'] == "depart") {
+          maneuver.add('0');
+        }
+        if (steps[i]['maneuver'] == "end-of-road") {
+          maneuver.add('2');
+        }
+        if (steps[i]['maneuver'] == "fork-right") {
+          maneuver.add('E');
+        }
+        if (steps[i]['maneuver'] == "fork-left") {
+          maneuver.add('Q');
+        }
+        if (steps[i]['maneuver'] == "merge") {
+          maneuver.add('W');
+        }
+        if (steps[i]['maneuver'] == "ramp-right") {
+          maneuver.add('D');
+        }
+        if (steps[i]['maneuver'] == "ramp-left") {
+          maneuver.add('A');
+        }
+        if (steps[i]['maneuver'] == "exit-right") {
+          maneuver.add('D');
+        }
+        if (steps[i]['maneuver'] == "exit-left") {
+          maneuver.add('A');
+        }
+        if (steps[i]['maneuver'] == "continue") {
+          maneuver.add('W');
+        }
+        if (steps[i]['maneuver'] == "roundabout") {
+          maneuver.add('D');
+        }
+        if (steps[i]['maneuver'] == "rotary") {
+          maneuver.add('D');
+        }
+        if (steps[i]['maneuver'] == "roundabout-left") {
+          maneuver.add('A');
+        }
+        if (steps[i]['maneuver'] == "roundabout-right") {
+          maneuver.add('D');
+        }
+        if (steps[i]['maneuver'] == "roundabout-alternative") {
+          maneuver.add('D');
+        }
+        if (steps[i]['maneuver'] == "exit-roundabout") {
+          maneuver.add('D');
+        }
+        if (steps[i]['maneuver'] == "exit-rotary") {
+          maneuver.add('D');
+        }
+        dis.add(steps[i]['distance']);
+      }
+    }
+
+    double checkDistance = await calculateDistance(positionBySteps);
+    print(checkDistance);
+    print(positionBySteps);
+    print(stepsCordi[0]);
+
+    if(checkDistance > checkDistancOne){
+      waitIfBigger++;
+      if(waitIfBigger == 10){
+        checkDistancOne = checkDistance;
+        waitIfBigger = 0;
+        stepsCordi = [];
+      }
+    }
+
+    checkDistancOne = checkDistance;
+
+    if (checkDistance < 0.05 ) {
+      print('The current location and destination location match: send 3, ${maneuver[positionBySteps]}, Position: ${positionBySteps}');
+      positionBySteps++;
+    } else if(checkDistance > 0.2) {
+      print('Distance > 200Meter : send 0');
+    } else if(checkDistance < 0.2 && checkDistance > 0.1){
+      print('Distance 200-100Meter : send 1');
+    } else if(checkDistance < 0.1 && checkDistance > 0.05){
+      print('Distance 100-20Meter : send 2');
+    }
+  }
+//calculate distance
+  Future<double> calculateDistance(positionBySteps) async {
+    var p = 0.017453292519943295;
+    var c = cos;
+    var a = 0.5 -
+        c((stepsCordi[positionBySteps].latitude - currentLocation!.latitude!) * p) / 2 +
+        c(currentLocation!.latitude! * p) *
+            c(stepsCordi[positionBySteps].latitude * p) *
+            (1 -
+                c((stepsCordi[positionBySteps].longitude - currentLocation!.longitude!) *
+                    p)) /
+            2;
+    return 12742 * asin(sqrt(a));
+  }
+
+
+
+
   @override
   void initState() {
     super.initState();
-
+    _location.onLocationChanged.listen((locationData) {
+      setState(() {
+        currentLocation = locationData;
+      });
+    });
     BluetoothConnection.toAddress(widget.server.address).then((_connection) {
       print('Connected to the device');
       connection = _connection;
@@ -66,13 +256,18 @@ class _ChatPage extends State<ChatPage> {
         }
       });
     }).catchError((error) {
-      print('Cannot connect, exception occured');
+      print('Cannot connect, exception occurred');
       print(error);
     });
   }
 
+  final _controller = TextEditingController();
+  final _controller1 = TextEditingController();
+
   @override
   void dispose() {
+    _controller.dispose();
+    _controller1.dispose();
     // Avoid memory leak (`setState` after dispose) and disconnect
     if (isConnected) {
       isDisconnecting = true;
@@ -85,79 +280,30 @@ class _ChatPage extends State<ChatPage> {
 
   @override
   Widget build(BuildContext context) {
-    final List<Row> list = messages.map((_message) {
-      return Row(
-        children: <Widget>[
-          Container(
-            child: Text(
-                (text) {
-                  return text == '/shrug' ? '¯\\_(ツ)_/¯' : text;
-                }(_message.text.trim()),
-                style: TextStyle(color: Colors.white)),
-            padding: EdgeInsets.all(12.0),
-            margin: EdgeInsets.only(bottom: 8.0, left: 8.0, right: 8.0),
-            width: 222.0,
-            decoration: BoxDecoration(
-                color:
-                    _message.whom == clientID ? Colors.blueAccent : Colors.grey,
-                borderRadius: BorderRadius.circular(7.0)),
-          ),
-        ],
-        mainAxisAlignment: _message.whom == clientID
-            ? MainAxisAlignment.end
-            : MainAxisAlignment.start,
-      );
-    }).toList();
-
-    final serverName = widget.server.name ?? "Unknown";
+    destination == ""
+        ? const Center()
+        : getRoute();
     return Scaffold(
       appBar: AppBar(
-          title: (isConnecting
-              ? Text('Connecting chat to ' + serverName + '...')
-              : isConnected
-                  ? Text('Live chat with ' + serverName)
-                  : Text('Chat log with ' + serverName))),
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Flexible(
-              child: ListView(
-                  padding: const EdgeInsets.all(12.0),
-                  controller: listScrollController,
-                  children: list),
-            ),
-            Row(
-              children: <Widget>[
-                Flexible(
-                  child: Container(
-                    margin: const EdgeInsets.only(left: 16.0),
-                    child: TextField(
-                      style: const TextStyle(fontSize: 15.0),
-                      controller: textEditingController,
-                      decoration: InputDecoration.collapsed(
-                        hintText: isConnecting
-                            ? 'Wait until connected...'
-                            : isConnected
-                                ? 'Type your message...'
-                                : 'Chat got disconnected',
-                        hintStyle: const TextStyle(color: Colors.grey),
-                      ),
-                      enabled: isConnected,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(8.0),
-                  child: IconButton(
-                      icon: const Icon(Icons.send),
-                      onPressed: isConnected
-                          ? () => _sendMessage(textEditingController.text)
-                          : null),
-                ),
-              ],
-            )
-          ],
+        title: SizedBox(
+          child: Text('Firefly'),
         ),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Ziel (Leerzeichen mit + ersetzen)',
+            ),
+            controller: _controller1,
+          ),
+          ElevatedButton(
+            onPressed: _sendMessage,
+            child: Text("Send Route"),
+          ),
+
+        ],
       ),
     );
   }
@@ -197,7 +343,7 @@ class _ChatPage extends State<ChatPage> {
             1,
             backspacesCounter > 0
                 ? _messageBuffer.substring(
-                    0, _messageBuffer.length - backspacesCounter)
+                0, _messageBuffer.length - backspacesCounter)
                 : _messageBuffer + dataString.substring(0, index),
           ),
         );
@@ -206,34 +352,33 @@ class _ChatPage extends State<ChatPage> {
     } else {
       _messageBuffer = (backspacesCounter > 0
           ? _messageBuffer.substring(
-              0, _messageBuffer.length - backspacesCounter)
+          0, _messageBuffer.length - backspacesCounter)
           : _messageBuffer + dataString);
     }
   }
 
-  void _sendMessage(String text) async {
-    text = text.trim();
-    textEditingController.clear();
+  void _sendMessage() async {
+    origin = "${currentLocation!.latitude!},${currentLocation!.longitude!}";
+    destination = _controller1.text;
+    await getRoute();
+    String message = ''; // Combine the message into a single string
 
-    if (text.length > 0) {
-      try {
-        connection!.output.add(Uint8List.fromList(utf8.encode(text + "\r\n")));
-        await connection!.output.allSent;
+    // Add the data to the message string
+    for (int i = 0; i < maneuver.length; i++) {
+      message += maneuver[i];
+    }
 
-        setState(() {
-          messages.add(_Message(clientID, text));
-        });
-
-        Future.delayed(Duration(milliseconds: 333)).then((_) {
-          listScrollController.animateTo(
-              listScrollController.position.maxScrollExtent,
-              duration: Duration(milliseconds: 333),
-              curve: Curves.easeOut);
-        });
-      } catch (e) {
-        // Ignore error, but notify state
-        setState(() {});
-      }
+    try {
+      BluetoothConnection connection =
+      await BluetoothConnection.toAddress("Bluetooth Device Address");
+      print('Connected to the device');
+      Uint8List bytes = Uint8List.fromList(utf8.encode(message)); // Convert the message to bytes
+      connection.output.add(bytes);
+      await connection.output.allSent;
+      await connection.finish();
+      print('Data sent over Bluetooth');
+    } catch (e) {
+      print('Error sending data over Bluetooth: $e');
     }
   }
 }
